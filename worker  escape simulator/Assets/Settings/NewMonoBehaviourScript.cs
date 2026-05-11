57b0e3797a6c11c1142ed23496010269f7e25424
Gabs8534 <gabriel61082896@alunos.sc.senac.br>
2026-05-11 15:41:04 -0300

    material

---
+++ worker  escape simulator/Assets/Settings/NewMonoBehaviourScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
